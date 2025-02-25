bf18f2b6d4c42a5986601864d6aa2a7b19f12df6
jourdelune <jourdelune863@gmail.com>
2025-02-25 22:15:57 +0100

    delete useless file

--- test.py
+++
@@ -1,5 +0,0 @@
-import shortlanguagedetection as ShortLanguageDetection
-
-detection = ShortLanguageDetection.Detector() # reliable_min=0.5 in arguments for less wrong detection.
-print(detection.detect('text'))
-# ('en', True)
