543798035f8f9f2bbcad9cadedea14cd14a43904
jourdelune <jourdelune863@gmail.com>
2025-02-27 14:39:13 +0100

    add more responsability to output class

--- src/short_language_detection/output_formater.py
+++ src/short_language_detection/output_formater.py
@@ -7,31 +7,30 @@ from typing import List, Tuple
 from .constants import LANGUAGES, TOP_LANGUAGES
 
 
-class Output:
+class OutputFormater:
     """
     The output of the language detection.
     """
 
     def __init__(
         self,
-        predictions: List[Tuple[str, float]],
+        detections_per_language: dict[str, int],
         reliability_threshold: float,
         filter_threshold: float,
+        equal_traitements: bool,
     ):
         """Initialize the output of the language detection.
 
         Args:
-            predictions (List[Tuple[str, float]]): The list of languages and their scores.
+            detections_per_language (dict[str, int]): the number of detections per language.
             reliability_threshold (float): the threshold to consider a language as reliable.
             filter_threshold (float): the threshold to filter the languages.
+            equal_traitements (bool): if True, all detectors are considered equally important.
         """
+        self._detections_per_language = detections_per_language
         self._reliability_threshold = reliability_threshold
         self._filter_threshold = filter_threshold
-
-        predictions = sorted(predictions, key=lambda x: x[1], reverse=True)
-        predictions = self._score_threshold(predictions, self._filter_threshold)
-
-        self._predictions = self._sort_by_language_popularity(predictions)
+        self._equal_traitements = equal_traitements
 
     def _score_threshold(
         self, scores: List[Tuple[str, float]], threshold: float = 0.0
@@ -77,16 +76,69 @@ class Output:
 
         return result
 
-    @property
-    def predictions(self) -> List[dict[str, float, bool]]:
-        """Return the predictions.
+    def _normalize_scores(
+        self, scores: List[Tuple[str, float]]
+    ) -> List[Tuple[str, float]]:
+        output = []
+        for lang, score in scores:
+            output.append((lang, score / self._detections_per_language[lang]))
+        return output
+
+    def _filter_unsupported_languages(
+        self, predictions: List[Tuple[str, float]]
+    ) -> List[Tuple[str, float]]:
+        return [
+            (lang, score)
+            for lang, score in predictions
+            if lang in self._detections_per_language
+        ]
+
+    def format_predictions(
+        self,
+        predictions: List[Tuple[str, float]],
+        apply_end_score_normalization: bool = True,
+    ) -> List[dict[str, float, bool]]:
+        """Formats the predictions of the language detection.
+
+        Args:
+            predictions (List[Tuple[str, float]]): The predictions of the language detection.
+            apply_end_score_normalization (bool, optional): If True, the scores are normalized by the number of detections for each language. Defaults to True.
 
         Returns:
-            List[Dict[str, float, bool]]: The list of languages and their scores.
+            List[dict[str, float, bool]]: The formatted predictions of the language detection.
         """
-        results = []
 
-        for i, (lang, score) in enumerate(self._predictions):
+        # unify the predictions
+        scores = {}
+        for prediction in predictions:
+            for lang, score in prediction:
+                if lang not in scores:
+                    scores[lang] = 0
+                scores[lang] += score
+
+        predictions = self._filter_unsupported_languages(list(scores.items()))
+
+        # Normalize the predictions scores by the number of detections for each language before sorting
+        # the order is important because each detector can have a different number of detections so scores can be more
+        # hurt by the normalization
+        if self._equal_traitements:
+            scores = self._normalize_scores(scores)
+
+        # Sort the predictions by score
+        predictions = sorted(predictions, key=lambda x: x[1], reverse=True)
+        predictions = self._score_threshold(predictions, self._filter_threshold)
+
+        predictions = self._sort_by_language_popularity(predictions)
+
+        # if not equal traitements, normalize the scores after sorting
+        # language with more detectors have advantage
+        if (
+            not self._equal_traitements and apply_end_score_normalization
+        ):  # when only one detector, no need to normalize
+            predictions = self._normalize_scores(predictions)
+
+        results = []
+        for i, (lang, score) in enumerate(predictions):
             results.append(
                 {
                     "language": str(LANGUAGES.from_iso_code_639_1_str(lang)),
